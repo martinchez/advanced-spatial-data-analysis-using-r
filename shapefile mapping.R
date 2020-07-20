e1849415c56f0f74340b086e5290e1a3994286d4
martinchez <martinchege1998@gmail.com>
2020-07-20 17:07:03 +0300

    shapefile mapping

--- shapefile mapping.R
+++ shapefile mapping.R
@@ -1,18 +1,20 @@
 library(raster)
 library(rgeos)
 library(maptools)
-
+library(sf)
+#install.packages("sf")
 setwd("C:\\Users\\ShellHunter.DESKTOP-EO423F6\\Documents\\rstudio\\Intermdiate spatial data analysis using R\\data\\fires")
 
 
 
 #Southeast_Asia_NOAA18_active_fires
-shapef<- readOGR("Southeast_Asia_NOAA18_active_fires")
+shapef<- rgdal::readOGR("Southeast_Asia_NOAA18_active_fires.shp")
 
+plot(shapef)
 
 
 setwd("C:/Users/ShellHunter.DESKTOP-EO423F6/Documents/rstudio/Intermdiate spatial data analysis using R/data/ggplot")
-?rgeos
+#?rgeos
 #VNM_adm2
-pl<- read
-plot(shapef)
+pl<- st_read("VNM_adm2.shp")
+plot(pl)
