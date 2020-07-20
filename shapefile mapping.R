0065163fe52ff4e4b7abcbf37ba8a39fbe83bb4d
martinchez <martinchege1998@gmail.com>
2020-07-20 16:20:15 +0300

    project inception

---
+++ shapefile mapping.R
@@ -0,0 +1,18 @@
+library(raster)
+library(rgeos)
+library(maptools)
+
+setwd("C:\\Users\\ShellHunter.DESKTOP-EO423F6\\Documents\\rstudio\\Intermdiate spatial data analysis using R\\data\\fires")
+
+
+
+#Southeast_Asia_NOAA18_active_fires
+shapef<- readOGR("Southeast_Asia_NOAA18_active_fires")
+
+
+
+setwd("C:/Users/ShellHunter.DESKTOP-EO423F6/Documents/rstudio/Intermdiate spatial data analysis using R/data/ggplot")
+?rgeos
+#VNM_adm2
+pl<- read
+plot(shapef)
